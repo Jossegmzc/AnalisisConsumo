fd05c6eeb81b67a864a7a3225e619aabcdef3f27
Jose Gomez <43909182+Jossegmzc@users.noreply.github.com>
2019-03-06 19:40:00 -0600

    Update tester2.java

--- src/analisisConsumo/tester2.java
+++ src/analisisConsumo/tester2.java
@@ -15,41 +15,32 @@ public class tester2 {
 	        File file = new File(fileNameDefined);
 	        HashMap<Integer,Double> datos = new HashMap<Integer,Double>();
 
-	        try{
-	        	
-	        	
-	            // -read from filePooped with Scanner class
-	            Scanner scanner = new Scanner(file);
-	            
+	        try{// -read from filePooped with Scanner class
+	            Scanner scanner = new Scanner(file);	            
 	            //scanner.useDelimiter(",");
 	    		scanner.nextLine();
 	            // hashNext() loops line-by-line
 	            while(scanner.hasNext()){
 	                //read single line, put in string
-	            	 String data = scanner.next();
-	            	 
+	            	 String data = scanner.next();	            	 
 	            	 String[] parts = data.split(",");
-	            	 int pos = Integer.parseInt(parts[0]); // 123 Integer.parseInt(parts[0])
-	            	 double gca = Double.parseDouble(parts[1]); // 654321 double value = Double.parseDouble(parts);
+	            	 int pos = Integer.parseInt(parts[0]); // 
+	            	 double gca = Double.parseDouble(parts[1]); // 
 	            	 System.out.printf("%s, %s \n", pos, gca);
 	            	 //System.out.println(pos);
-	            	 //System.out.println(gca);
-	            	     	 
-	            //	datos.put(Integer.parseInt(data.split(",")[0]), Double.parseDouble(data.split(",")[1]));
+	            	 //System.out.println(gca);	            	     	 
+	            	 //	datos.put(Integer.parseInt(data.split(",")[0]), Double.parseDouble(data.split(",")[1]));
 
-	            	// System.out.println(data);
-	            	 //System.out.println();
+	            	 // System.out.println(data);
+	            	
 	            }
 	            for (HashMap.Entry<Integer, Double> entry : datos.entrySet()) {
 	            	System.out.printf("key: %s, value: %s", entry.getKey(), entry.getValue());
-
 	            }
 	            // after loop, close scanner
 	            scanner.close();
 
-
 	        }catch (FileNotFoundException e){
-
 	            e.printStackTrace();
 	        }
 
